0dc6f9710b889b59c034afc6ad75b003b05dad81
ProggDM <dama99428@gmail.com>
2023-04-21 19:01:25 +0300

    commit

--- assets/Scripts/GameObjects/Barracks/BarracksLogic.ts
+++ assets/Scripts/GameObjects/Barracks/BarracksLogic.ts
@@ -12,7 +12,7 @@ export class BarracksLogic extends Component {
 
     public createTroop() {
         console.log("createTroop");
-        SpawnObjects.instance.spawnObjectsNearby(TypesObjects.TROOP_OVERLAND, 1, this.objectParameters.index, 2);
+        SpawnObjects.instance.spawnObjectsPos(TypesObjects.TROOP_OVERLAND, 1, 0);
     }
 }
 
