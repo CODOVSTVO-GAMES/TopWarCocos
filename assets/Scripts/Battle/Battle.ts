f3bc13623ce16c5a7317ea0fa15ac56e027fba76
ProggDM <dama99428@gmail.com>
2023-04-27 18:35:22 +0300

    battle

--- assets/Scripts/Battle/Battle.ts
+++ assets/Scripts/Battle/Battle.ts
@@ -3,6 +3,7 @@ import { TypesObjects } from '../Static/TypesObjects';
 import { TroopRender } from './TroopRender';
 import { SpriteStorage } from '../SpriteStorage';
 import { BattleMap } from './BattleMap';
+import { TypesAttack } from '../Static/TypesAttack';
 const { ccclass, property } = _decorator;
 
 @ccclass('Battle')
@@ -12,6 +13,9 @@ export class Battle extends Component {
 
     @property({ type: Prefab })
     public troop: Prefab;
+
+    @property({ type: Node })
+    public inBattleBtn: Node;
 
     @property({ type: Node })
     public cards: Node[] = [];
@@ -28,6 +32,9 @@ export class Battle extends Component {
     public arrayOwn: Unit[] = [];
     public arrayEnemy: Unit[] = [];
     public arrayCards: FreeUnit[] = [];
+    public isBattle: boolean = false;
+    public attackingTeam: number = 0;
+    public attackNumber: number = 0;
 
     onLoad() {
         Battle.instance = this;
@@ -35,6 +42,7 @@ export class Battle extends Component {
     }
 
     start() {
+        this.arrayEnemy = this.genEnemyUnits();
         this.arrayCards = this.getFreeUnits();
         this.enemyRender();
         this.cardsRender();
@@ -50,20 +58,22 @@ export class Battle extends Component {
 
     genEnemyUnits(): Unit[] {
         let array = new Array(6);
-        for (let i = 0; i < array.length; i++) {
-            array[i] = new Unit(10, 0, "vertical", i, TypesObjects.TROOP_OVERLAND, i + 1);
+        for (let i = 0; i < 2; i++) {
+            array[i] = new Unit(10, 2, "vertical", i, TypesObjects.TROOP_OVERLAND, i + 1);
         }
         return array;
     }
 
     enemyRender() {
-        this.arrayEnemy = this.genEnemyUnits();
         for (let i = 0; i < this.arrayEnemy.length; i++) {
-            let gameObject = instantiate(this.troop);
-            gameObject.setParent(BattleMap.instance.coordsEnemy[i]);
-            let troopRender = gameObject.getComponent(TroopRender);
-            troopRender.index = i;
-            troopRender.team = TypesObjects.TEAM_ENEMY;
+            if (this.arrayEnemy[i] != null) {
+                let gameObject = instantiate(this.troop);
+                gameObject.setParent(BattleMap.instance.coordsEnemy[i]);
+                let troopRender = gameObject.getComponent(TroopRender);
+                troopRender.index = i;
+                troopRender.team = TypesObjects.TEAM_ENEMY;
+                this.arrayEnemy[i].link = troopRender;
+            }
         }
     }
 
@@ -85,7 +95,7 @@ export class Battle extends Component {
         for (let i = 0; i < this.arrayOwn.length; i++) {
             if (this.arrayOwn[i] == null) {
                 let unit = this.arrayCards[customEventData];
-                this.arrayOwn[i] = new Unit(unit.hp, 0, "vertical", i, unit.type, unit.level);
+                this.arrayOwn[i] = new Unit(unit.hp, 2, "vertical", i, unit.type, unit.level);
                 if (unit.quantity > 1) {
                     unit.quantity--;
                 }
@@ -110,6 +120,10 @@ export class Battle extends Component {
                     this.arrayOwn[i].link.nodeObject.destroy();
                     this.spawnTroop(i);
                 }
+                if (this.arrayOwn[i].hp <= 0) {
+                    this.arrayOwn[i].link.nodeObject.destroy();
+                    this.arrayOwn[i] = null;
+                }
             }
         }
     }
@@ -118,7 +132,7 @@ export class Battle extends Component {
         let gameObject = instantiate(this.troop);
         gameObject.setParent(BattleMap.instance.coordsOwn[i]);
         this.arrayOwn[i].link = gameObject.getComponent(TroopRender);
-        this.arrayOwn[i].link.index = this.arrayOwn[i].numInCell;
+        this.arrayOwn[i].link.index = this.arrayOwn[i].index;
         this.arrayOwn[i].link.team = TypesObjects.TEAM_OWN;
     }
 
@@ -146,28 +160,140 @@ export class Battle extends Component {
     }
 
     inBattle() {
-        // запретить любые нажатия на экран
-        // запуск куратины
-        // 
+        if (this.isBattle == false) {
+            for (let i = 0; i < this.arrayOwn.length; i++) {
+                if (this.arrayOwn[i] != null) {
+                    this.isBattle = true;
+                    this.inBattleBtn.active = false;
+                    for (let i = 0; i < this.cards.length; i++) {
+                        this.cards[i].active = false;
+                    }
+                    this.attack();
+                    // while (this.troopAlive()) {
+
+                    // }
+
+                    // for (let i = 0; i < 1; i++) {
+
+                    // }}
+                }
+            }
+        }
     }
 
-    
+    attack() {
+        if (this.attackingTeam == 0) {
+            for (let i = 0; i < this.arrayEnemy.length; i++) {
+                if (i == 0) {
+                    this.attackNumber++;
+                }
+                if (this.arrayEnemy[i] != null) {
+                    if (this.arrayEnemy[i].attackNumber < this.attackNumber) {
+                        let units = this.goalSelection(this.arrayEnemy[i].link.team, this.arrayEnemy[i].type, "hor", "one");
+                        if (units.length > 0) {
+                            for (let i = 0; i < units.length; i++) {
+                                this.arrayOwn[units[i]].hp -= this.arrayEnemy[i].damage;
+                            }
+                        }
+                        this.unitsOwnRender();
+                        this.arrayEnemy[i].attackNumber = this.attackNumber;
+                    }
+                }
+            }
+        }
+        else if (this.attackingTeam == 1) {
+
+        }
+        console.log(this.arrayEnemy);
+        console.log(this.arrayOwn);
+    }
+
+    troopAlive(): boolean {
+        return this.howManyAliveOwn() > 0 && this.howManyAliveEnemy() > 0;
+    }
+
+    howManyAliveOwn(): number {
+        let quantityAlive = 0;
+        for (let i = 0; i < this.arrayOwn.length; i++) {
+            if (this.arrayOwn[i] != null) {
+                if (this.arrayOwn[i].hp > 0) {
+                    quantityAlive++;
+                }
+            }
+        }
+        return quantityAlive;
+    }
+
+    howManyAliveEnemy(): number {
+        let quantityAlive = 0;
+        for (let i = 0; i < this.arrayEnemy.length; i++) {
+            if (this.arrayEnemy[i] != null) {
+                if (this.arrayEnemy[i].hp > 0) {
+                    quantityAlive++;
+                }
+            }
+        }
+        return quantityAlive;
+    }
+
+    goalSelection(team: string, type: string, typeAttack: string, typeShot: string): number[] {
+        let units;
+        let quantity;
+        let arrayUnits = [];
+        if (team == TypesObjects.TEAM_ENEMY) {
+            units = this.arrayOwn.slice(0);
+        }
+        else if (team == TypesObjects.TEAM_OWN) {
+            units = this.arrayEnemy.slice(0);
+        }
+        switch (typeShot) {
+            case TypesAttack.ONE:
+                quantity = 1;
+                break;
+            case TypesAttack.TWO:
+                quantity = 2;
+                break;
+            case TypesAttack.THREE:
+                quantity = 3;
+                break;
+        }
+        for (let i = 0; i < units.length; i++) {
+            if (units[i] != null) {
+                if (typeAttack == TypesAttack.HORIZON) {
+                    if (units[i].type == type) {
+                        arrayUnits.push(i);
+                    }
+                }
+                else if (typeAttack == TypesAttack.VERTICAL) {
+                    arrayUnits.push(i);
+                }
+            }
+        }
+        let conclusion = [];
+        for (let i = 0; i < quantity; i++) {
+            if (arrayUnits.length > 0) {
+                conclusion.push(arrayUnits[Math.floor(Math.random() * arrayUnits.length)]);
+            }
+        }
+        return conclusion;
+    }
 }
 
 export class Unit {
     hp: number;
     damage: number;
     skill: string;
-    numInCell: number;
+    index: number;
     type: string;
     level: number;
+    attackNumber: number = 0;
     link: TroopRender;
 
-    constructor(hp: number, damage: number, skill: string, numInCell: number, type: string, level: number) {
+    constructor(hp: number, damage: number, skill: string, index: number, type: string, level: number) {
         this.hp = hp
         this.damage = damage;
         this.skill = skill;
-        this.numInCell = numInCell;
+        this.index = index;
         this.type = type;
         this.level = level;
     }
