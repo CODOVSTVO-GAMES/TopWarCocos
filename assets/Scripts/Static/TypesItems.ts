afa35caaa479baf2e1344e0fb152e25344933c80
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2023-05-05 17:14:52 +0300

    config

--- assets/Scripts/Static/TypesItems.ts
+++ assets/Scripts/Static/TypesItems.ts
@@ -3,17 +3,15 @@ export class TypesItems {
     public static PLAN_MAX_MARINE: string = "pmm";
     public static PLAN_MAX_AIR: string = "pma";
     public static PLAN_MAX_MINE: string = "pmm";
-
-    public static PLAN_MERGE_BARRACK: string = "pmeb";
-    public static PLAN_MERGE_ROPE: string = "pmer";
-    public static PLAN_MERGE_AIRPORT: string = "pmea";
+    public static PLAN_MAX_BARRACK: string = "pmb";
+    public static PLAN_MAX_ROPE: string = "pmr";
+    public static PLAN_MAX_AIRPORT: string = "pmair";
+    public static PLAN_MAX_MAINBUILDING: string = "pmb";
 
     public static PLAN_CREATE_BARRACK: string = "pcb";
     public static PLAN_CREATE_ROPE: string = "pcr";
     public static PLAN_CREATE_AIRPORT: string = "pca";
     public static PLAN_CREATE_MINE: string = "pcm";
-
-    public static PLAN_TOWN_HALL: string = "pmb";
 
     public static BOOK_EXP_GREEN: string = "beg";
     public static BOOK_EXP_WHITE: string = "bew";
