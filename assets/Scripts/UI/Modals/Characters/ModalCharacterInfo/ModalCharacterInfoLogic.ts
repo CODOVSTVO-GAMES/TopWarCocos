e533995bf58a6acb20846e4d6eca40b0a565c9ea
ProggDM <dama99428@gmail.com>
2023-05-12 19:45:44 +0300

    characters

--- assets/Scripts/UI/Modals/Characters/ModalCharacterInfo/ModalCharacterInfoLogic.ts
+++ assets/Scripts/UI/Modals/Characters/ModalCharacterInfo/ModalCharacterInfoLogic.ts
@@ -2,6 +2,7 @@ import { _decorator, Component, Node } from 'cc';
 import { ModalCharacterPumpingInterface } from '../ModalCharacterPumping/ModalCharacterPumpingInterface';
 import { ModalCharacterInfoIntarface } from './ModalCharacterInfoInterface';
 import { TypesModalPumping } from '../../../../Static/TypesModalPumping';
+import { ModalCharacterPumpingLogic } from '../ModalCharacterPumping/ModalCharacterPumpingLogic';
 const { ccclass, property } = _decorator;
 
 @ccclass('ModalCharacterInfoLogic')
@@ -13,7 +14,15 @@ export class ModalCharacterInfoLogic extends Component {
     @property({ type: Node })
     public modalPumping: Node;
 
+    public characterIndex: number;
+
+    start() {
+        this.modal.active = false;
+        this.modalPumping.active = false;
+    }
+
     modalOpen(event, customEventData) {
+        this.characterIndex = customEventData;
         this.modal.active = ModalCharacterInfoIntarface.instance.renderCharacter(customEventData);
     }
 
@@ -21,8 +30,21 @@ export class ModalCharacterInfoLogic extends Component {
         this.modal.active = false;
     }
 
-    modalPumpingOpen() {
+    modalParametersOpen() {
+        ModalCharacterPumpingInterface.instance.renderModalPumping(TypesModalPumping.PARAMETERS);
+        ModalCharacterPumpingLogic.instance.characterIndex = this.characterIndex;
+        this.modalPumping.active = true;
+    }
+
+    modalPumpingLevelOpen() {
         ModalCharacterPumpingInterface.instance.renderModalPumping(TypesModalPumping.PUMPING_LEVEL);
+        ModalCharacterPumpingLogic.instance.characterIndex = this.characterIndex;
+        this.modalPumping.active = true;
+    }
+
+    modalPumpingStarsOpen() {
+        ModalCharacterPumpingInterface.instance.renderModalPumping(TypesModalPumping.PUMPING_STARS);
+        ModalCharacterPumpingLogic.instance.characterIndex = this.characterIndex;
         this.modalPumping.active = true;
     }
 
--- assets/Scripts/UI/Modals/Characters/ModalCharacterPumping/ModalCharacterPumpingLogic.ts
+++ assets/Scripts/UI/Modals/Characters/ModalCharacterPumping/ModalCharacterPumpingLogic.ts
@@ -4,7 +4,13 @@ const { ccclass, property } = _decorator;
 @ccclass('ModalCharacterPumpingLogic')
 export class ModalCharacterPumpingLogic extends Component {
 
+    public static instance: ModalCharacterPumpingLogic;
+
     public characterIndex: number;
+
+    onLoad() {
+        ModalCharacterPumpingLogic.instance = this;
+    }
 
     saveCharacter(index: number) {
         this.characterIndex = index;
