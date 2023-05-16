104538b12e72cca6b4a8507234a24ccda2ac7730
ProggDM <dama99428@gmail.com>
2023-05-16 20:45:56 +0300

    commit

--- assets/Scripts/Static/TechnicalConfig.ts
+++
@@ -1,8 +0,0 @@
-export class TechnicalConfig {
-    public static NAME: string = 'office'
-    // public static SERVER_DOMAIN : string = 'http://localhost:9600/' 
-    public static SERVER_DOMAIN: string = 'https://codovstvo.ru:9600/'
-    
-    public static ISPROD = false;
-    // public static ISPROD = true;
-}
