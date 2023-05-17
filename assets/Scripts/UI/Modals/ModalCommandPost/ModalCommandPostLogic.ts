fef0ddc400d10def4504479aa07bc6b3003913da
Trer0c <v.smirnov@aplink.ru>
2023-05-17 20:47:37 +0300

    commit

--- assets/Scripts/UI/Modals/ModalCommandPost/ModalCommandPostLogic.ts
+++ assets/Scripts/UI/Modals/ModalCommandPost/ModalCommandPostLogic.ts
@@ -5,16 +5,32 @@ const { ccclass, property } = _decorator;
 export class ModalCommandPostLogic extends Component {
 
     @property({ type: Node })
+    public backgraund: Node;
+
+    @property({ type: Node })
     public upgrateCommandPost: Node;
 
     @property({ type: Node })
     public upgrateOther: Node;
 
+    start() {
+        this.closeAll();
+    }
+
     openUpgradeCommandPost() {
+        this.backgraund.active = true;
         this.upgrateCommandPost.active = true;
     }
 
     openUpgradeOther(event, customEventData) {
+        console.log(customEventData);
+        this.backgraund.active = true;
         this.upgrateOther.active = true;
+    }
+
+    closeAll() {
+        this.backgraund.active = false;
+        this.upgrateCommandPost.active = false;
+        this.upgrateOther.active = false;
     }
 }
