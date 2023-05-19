57d20a83261d175573eefe3088dfcc643a3cbcec
Trer0c <v.smirnov@aplink.ru>
2023-05-19 14:52:30 +0300

    commit

--- assets/Scripts/UI/Modals/ModalCommandPost/ModalCommandPostLogic.ts
+++ assets/Scripts/UI/Modals/ModalCommandPost/ModalCommandPostLogic.ts
@@ -6,6 +6,8 @@ const { ccclass, property } = _decorator;
 @ccclass('ModalCommandPostLogic')
 export class ModalCommandPostLogic extends Component {
 
+    public static instance: ModalCommandPostLogic;
+
     @property({ type: Node })
     public backgraund: Node;
 
@@ -15,6 +17,12 @@ export class ModalCommandPostLogic extends Component {
     @property({ type: Node })
     public upgrateOther: Node;
 
+    public typeActiveModal: string;
+
+    onLoad() {
+        ModalCommandPostLogic.instance = this;
+    }
+
     start() {
         this.closeAll();
     }
@@ -23,60 +31,78 @@ export class ModalCommandPostLogic extends Component {
         UpgradeCommandPostInerface.instance.updateInterface();
         this.backgraund.active = true;
         this.upgrateCommandPost.active = true;
+        this.typeActiveModal = "upgradeCommandPost";
+    }
+
+    openRepairShop() {
+
+        this.typeActiveModal = "repairShop";
+        this.openUpgradeOther();
     }
 
     openMergeGoldMine() {
         UpgradeOtherInterface.instance.updateInterfaceMergeGoldMine();
+        this.typeActiveModal = "mergeGoldMine";
         this.openUpgradeOther();
     }
 
     openMergeTroopAir() {
         UpgradeOtherInterface.instance.updateInterfaceMergeTroopAir();
+        this.typeActiveModal = "mergeTroopAir";
         this.openUpgradeOther();
     }
 
     openMergeTroopMarine() {
         UpgradeOtherInterface.instance.updateInterfaceMergeTroopMarine();
+        this.typeActiveModal = "mergeTroopMarine";
         this.openUpgradeOther();
     }
 
     openMergeTroopOverland() {
-        UpgradeOtherInterface.instance.updateInterfaceMergeBarracksOverland();
+        UpgradeOtherInterface.instance.updateInterfaceMergeTroopOverland();
+        this.typeActiveModal = "mergeTroopOverland";
         this.openUpgradeOther();
     }
 
     openMergeBarracksAir() {
         UpgradeOtherInterface.instance.updateInterfaceMergeBarracksAir();
+        this.typeActiveModal = "mergeBarracksAir";
         this.openUpgradeOther();
     }
 
     openMergeBarracksMarine() {
         UpgradeOtherInterface.instance.updateInterfaceMergeBarracksMarine();
+        this.typeActiveModal = "mergeBarracksMarine";
         this.openUpgradeOther();
     }
 
     openMergeBarracksOverland() {
         UpgradeOtherInterface.instance.updateInterfaceMergeBarracksOverland();
+        this.typeActiveModal = "mergeBarracksOverland";
         this.openUpgradeOther();
     }
 
     openBuildGoldMine() {
         UpgradeOtherInterface.instance.updateInterfaceBuildGoldMine();
+        this.typeActiveModal = "buildGoldMine";
         this.openUpgradeOther();
     }
 
     openBuildBarracksAir() {
         UpgradeOtherInterface.instance.updateInterfaceBuildBarracksAir();
+        this.typeActiveModal = "buildBarracksAir";
         this.openUpgradeOther();
     }
 
     openBuildBarracksMarine() {
         UpgradeOtherInterface.instance.updateInterfaceBuildBarracksMarine();
+        this.typeActiveModal = "buildBarracksMarine";
         this.openUpgradeOther();
     }
 
     openBuildBarracksOverland() {
         UpgradeOtherInterface.instance.updateInterfaceBuildBarracksOverland();
+        this.typeActiveModal = "buildBarracksOverland";
         this.openUpgradeOther();
     }
 
--- assets/Scripts/UI/Modals/ModalCommandPost/UpgradePanel/UpgradeOtherLogic.ts
+++ assets/Scripts/UI/Modals/ModalCommandPost/UpgradePanel/UpgradeOtherLogic.ts
@@ -1,11 +1,87 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component } from 'cc';
+import { ControllerCommandPostStorage } from '../../../../Storage/Controllers/ControllerCommandPostStorage';
+import { UpgradeOtherInterface } from './UpgradeOtherInterface';
+import { ControllerGameStorage } from '../../../../Storage/Controllers/ControllerGameStorage';
 const { ccclass, property } = _decorator;
 
 @ccclass('UpgradeOtherLogic')
 export class UpgradeOtherLogic extends Component {
 
-    upgrateObject() {
-        
+    upgrateMergeGoldMine() {
+        ControllerGameStorage.addExperience(1);
+        ControllerGameStorage.addTechnoPower(1);
+        ControllerCommandPostStorage.addLevelMergeGoldMine();
+        UpgradeOtherInterface.instance.updateInterfaceMergeGoldMine();
+    }
+
+    upgrateMergeTroopAir() {
+        ControllerGameStorage.addExperience(1);
+        ControllerGameStorage.addTechnoPower(1);
+        ControllerCommandPostStorage.addLevelMergeTroopAir();
+        UpgradeOtherInterface.instance.updateInterfaceMergeTroopAir();
+    }
+
+    upgrateMergeTroopMarine() {
+        ControllerGameStorage.addExperience(1);
+        ControllerGameStorage.addTechnoPower(1);
+        ControllerCommandPostStorage.addLevelMergeTroopMarine();
+        UpgradeOtherInterface.instance.updateInterfaceMergeTroopMarine();
+    }
+
+    upgrateMergeTroopOverland() {
+        ControllerGameStorage.addExperience(1);
+        ControllerGameStorage.addTechnoPower(1);
+        ControllerCommandPostStorage.addLevelMergeTroopOverland();
+        UpgradeOtherInterface.instance.updateInterfaceMergeTroopOverland();
+    }
+
+    upgrateMergeBarracksAir() {
+        ControllerGameStorage.addExperience(1);
+        ControllerGameStorage.addTechnoPower(1);
+        ControllerCommandPostStorage.addLevelMergeBarracksAir();
+        UpgradeOtherInterface.instance.updateInterfaceMergeBarracksAir();
+    }
+
+    upgrateMergeBarracksMarine() {
+        ControllerGameStorage.addExperience(1);
+        ControllerGameStorage.addTechnoPower(1);
+        ControllerCommandPostStorage.addLevelMergeBarracksMarine();
+        UpgradeOtherInterface.instance.updateInterfaceMergeBarracksMarine();
+    }
+
+    upgrateMergeBarracksOverland() {
+        ControllerGameStorage.addExperience(1);
+        ControllerGameStorage.addTechnoPower(1);
+        ControllerCommandPostStorage.addLevelMergeBarracksOverland();
+        UpgradeOtherInterface.instance.updateInterfaceMergeBarracksOverland();
+    }
+
+    upgrateBuildGoldMine() {
+        ControllerGameStorage.addExperience(1);
+        ControllerGameStorage.addTechnoPower(1);
+        ControllerCommandPostStorage.addLevelBuildGoldMine();
+        UpgradeOtherInterface.instance.updateInterfaceBuildGoldMine();
+    }
+
+    upgrateBuildBarracksAir() {
+        ControllerGameStorage.addExperience(1);
+        ControllerGameStorage.addTechnoPower(1);
+        ControllerCommandPostStorage.addLevelBuildBarracksAir();
+        UpgradeOtherInterface.instance.updateInterfaceBuildBarracksAir();
+    }
+
+    upgrateBuildBarracksMarine() {
+        ControllerGameStorage.addExperience(1);
+        ControllerGameStorage.addTechnoPower(1);
+        ControllerCommandPostStorage.addLevelBuildBarracksMarine();
+        UpgradeOtherInterface.instance.updateInterfaceBuildBarracksMarine();
+    }
+
+    upgrateBuildBarracksOverland() {
+        ControllerGameStorage.addExperience(1);
+        ControllerGameStorage.addTechnoPower(1);
+        ControllerCommandPostStorage.addLevelBuildBarracksOverland();
+        UpgradeOtherInterface.instance.updateInterfaceBuildBarracksOverland();
     }
 
     getItems() {
