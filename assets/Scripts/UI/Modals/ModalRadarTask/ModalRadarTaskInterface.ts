9543ae2a7e1d8f32f8f5075df7404b4c1d364a35
ProggDM <dama99428@gmail.com>
2023-05-25 17:05:13 +0300

    radar

--- assets/Scripts/UI/Modals/ModalRadarTask/ModalRadarTaskInterface.ts
+++ assets/Scripts/UI/Modals/ModalRadarTask/ModalRadarTaskInterface.ts
@@ -1,6 +1,10 @@
-import { _decorator, Color, Component, Label, Node, Sprite } from 'cc';
+import { _decorator, Color, Component, Label, Node, Sprite, SpriteFrame } from 'cc';
 import { RadarTask } from '../../../Structures/RadarTask';
 import { TypesRadar } from '../../../Static/TypesRadar';
+import { TypesItems } from '../../../Static/TypesItems';
+import { SecondaryInterface } from '../../SecondaryInterface';
+import { TypesModals } from '../../../Static/TypesModals';
+import { ModalRadarTaskLogic } from './ModalRadarTaskLogic';
 const { ccclass, property } = _decorator;
 
 @ccclass('ModalRadarTaskInterface')
@@ -29,23 +33,42 @@ export class ModalRadarTaskInterface extends Component {
     @property({ type: Sprite })
     public rewards: Sprite[] = [];
 
+    @property({ type: SpriteFrame })
+    public spritesBG: SpriteFrame[] = [];
+
+    @property({ type: SpriteFrame })
+    public rewardSprites: SpriteFrame[] = [];
+
     onLoad() {
         ModalRadarTaskInterface.instance = this;
     }
 
     updateInterface(task: RadarTask) {
+        ModalRadarTaskLogic.instance.task = task;
         this.title.string = "Задание завершится через " + task.time;
         this.status.string = "Не началось";
-        this.image.color = this.getSprite(task.type);
+        this.image.color = this.getSpriteTask(task.type);
         for (let i = 0; i < this.stars.length; i++) {
             this.stars[i].active = i < task.stars;
         }
-        for(let i = 0; i < this.quantity.length; i++) {
-
+        for (let i = 0; i < task.rewards.length; i++) {
+            this.quantity[i].string = task.rewards[i].quantity.toString();
+            switch (task.rewards[i].type) {
+                case TypesItems.PLAN_MAX_MAINBUILDING:
+                    this.rewardsBG[i].spriteFrame = this.spritesBG[0];
+                    break;
+                case TypesItems.EXPERIENCE:
+                    this.rewardsBG[i].spriteFrame = this.spritesBG[1];
+                    break;
+                default:
+                    this.rewardsBG[i].spriteFrame = this.spritesBG[2];
+                    break;
+            }
         }
+        this.startTimer();
     }
 
-    getSprite(type: string): Color {
+    getSpriteTask(type: string): Color {
         switch (type) {
             case TypesRadar.TASK_SALVATION:
                 return new Color(255, 0, 0, 255);
@@ -54,5 +77,17 @@ export class ModalRadarTaskInterface extends Component {
             case TypesRadar.TASK_PERSONAL:
                 return new Color(0, 0, 255, 255);
         }
+    }
+
+    startTimer() {
+        let timer = setInterval(() => {
+            let time = ModalRadarTaskLogic.instance.task.time;
+            if (SecondaryInterface.instance.getTypeActiveModal() == TypesModals.RADAR && time > 0) {
+                this.title.string = "Задание завершится через " + time;
+            }
+            else {
+                clearInterval(timer);
+            }
+        }, 1000);
     }
 }
--- assets/Scripts/UI/Modals/ModalRadarTask/ModalRadarTaskLogic.ts
+++ assets/Scripts/UI/Modals/ModalRadarTask/ModalRadarTaskLogic.ts
@@ -1,4 +1,5 @@
 import { _decorator, Component, Node } from 'cc';
+import { RadarTask } from '../../../Structures/RadarTask';
 const { ccclass, property } = _decorator;
 
 @ccclass('ModalRadarTaskLogic')
@@ -6,9 +7,13 @@ export class ModalRadarTaskLogic extends Component {
 
     public static instance: ModalRadarTaskLogic;
 
-    
+    public task: RadarTask
 
     onLoad() {
         ModalRadarTaskLogic.instance = this;
+    }
+
+    pushButton() {
+
     }
 }
