4843f0305d2d5ea9024d798d204b5062a937da4b
ProggDM <dama99428@gmail.com>
2023-06-06 20:18:21 +0300

    commit

--- assets/Scripts/Animations/GameObjects/FlightGameObjects.ts
+++ assets/Scripts/Animations/GameObjects/FlightGameObjects.ts
@@ -1,4 +1,5 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Vec3 } from 'cc';
+import { ControllerHomeMapStorage } from '../../Storage/Controllers/ControllerHomeMapStorage';
 const { ccclass, property } = _decorator;
 
 @ccclass('FlightGameObjects')
@@ -6,22 +7,31 @@ export class FlightGameObjects extends Component {
 
     public static instance: FlightGameObjects;
 
-    @property({ type: Node })
-    public object: Node;
-
+    private object: Node;
     private trigger: boolean = false;
+    private speed: number = 2;
+    private toPos: Vec3;
 
     onLoad() {
         FlightGameObjects.instance = this;
     }
 
     moveToCell(object: Node, index: number) {
-
+        this.object = object;
+        this.toPos = ControllerHomeMapStorage.getCoordPosition(index);
     }
 
-    update(dt: number) {
-        if (this.trigger == true) {
-            // this.object.
-        }
+    // update(dt: number) {
+    //     if (this.trigger == true) {
+    //         console.log(dt * this.speed)
+    //         this.object.position = new Vec3(
+    //             this.getLerp(this.object.position.x, 100, dt * this.speed),
+    //             this.getLerp(this.object.position.y, 200, dt * this.speed),
+    //             0);
+    //     }
+    // }
+
+    getLerp(one: number, two: number, current: number) {
+        return one + (two - one) * current;
     }
 }
