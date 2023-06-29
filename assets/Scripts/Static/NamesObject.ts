347f091dd6f34eafb6eea0f643d612bcaecd20fe
Talis54 <starhawk01@bk.ru>
2023-06-29 17:00:40 +0300

    Commit

---
+++ assets/Scripts/Static/NamesObject.ts
@@ -0,0 +1,34 @@
+export class NamesObject {
+    //только для обычных обьектов на карте
+    public static NAME_COMMAND_POST: string = "Командный Пост"
+    public static NAME_GOLD_MINE: string = "Золотой Рудник"
+    public static NAME_BANK: string = "Банк"
+    public static NAME_AUTOCOMBINE: string = "Автокомбайн"
+    public static NAME_RADAR: string = "Радар"
+    public static NAME_TREASURES: string = "Сокровища"
+    public static WHOLE_MANIPULATOR: string = "Манипулятор"
+    public static PADDED_MANIPULATOR: string = "Манипулятор"
+    public static REPAIR_SHOP: string = "Ремонтный Цех"
+    public static LOBBY_WARS: string = "Лобби Войны"
+    public static EXPEDITION: string = "Экспедиционная База"
+
+
+
+    //для воинов на карте
+    public static NAME_TROOP_AIR: string[] = [
+        "", //1уровень
+        "",// 2
+        "",//3
+        "млыщпоащоппш",
+    ]
+
+    public static NAME_TROOP_MARINE: string[] = [
+        "",
+        ""
+    ]
+
+    public static NAME_TROOP_OVERLAND: string[] = [
+        "",
+        ""
+    ]
+}
